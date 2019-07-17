e3e1d94b86d75f1486f5af111ef7f8f17066ac37
ajaythanki <akthanki5@gmail.com>
2019-07-17 22:54:35 +0530

    done upto lesson30

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+ReactDOM.render(<Counter />, document.getElementById("root"));
+serviceWorker.unregister();
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+// import { Teacher } from './teacher';
+// const element = <h1>Hello world!</h1>;
+// console.log(element);
+// ReactDOM.render(element, document.getElementById("root"));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// const colors = ['red','green','blue'];
+// const list = colors.map(color => `<li>${color}</li>`);
+// console.log(list);
 
-const element = <h1>Hello world!</h1>;
-console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
+// const country = {
+//     street:'abcd',
+//     city:'xxx',
+//     pin:'123'
+// }
+
+// console.log(`contry obj: ${country}`);
+
+// const {street, city, pin} = country;
+// console.log(`Street var: ${street}`);
+// console.log(`City var: ${city}`);
+// console.log(`Pin var: ${pin}`);
+
+// const teacher = new Teacher('ajay','bca');
+// teacher.teach(); 
---
+++ react-app/src/teacher.js
@@ -0,0 +1,11 @@
+import {Person} from './person';
+function promote(){}
+export class Teacher extends Person{
+    constructor(name, degree){
+        super(name);
+        this.degree = degree;
+    }
+    teach(){
+        console.log('teach');
+    }
+}
