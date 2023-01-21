deb803ac9282e96bb4e3267072eaec1493df22d4
Ajay Thanki <akthanki5@gmail.com>
2023-01-21 19:23:25 +0530

    Converted to functional component and implemented movies card

--- react-app/src/components/vidly.jsx
+++ react-app/src/components/vidly.jsx
@@ -1,67 +1,74 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like";
-class Vidly extends Component {
-  state = { movies: getMovies() };
+import Card from "./card";
+// import Like from "./common/like";
+const Vidly = () => {
+  // state = { movies: getMovies() };
+  const [movies, setMovies] = useState(getMovies())
+  // componentDidUpdate(prevProps, prevState) {
+  //   // console.log("prevProps", prevProps);
+  //   // console.log("prevState", prevState);
+  //   if (prevState.movies.value !== movies.value) {
+  //     //Ajax call
+  //     console.log(movies)
+  //     // this.setState({ movies:this.movies });
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies });
+  //   }
+  // }
+  const handleDelete = (movie) => {
+    const newMovies = movies.filter((m) => m._id !== movie._id);
+    setMovies( newMovies);
   };
 
-  handleLike = movie => {
-    const movies = [...this.state.movies];
-    const index = movies.indexOf(movie);
-    movies[index] = { ...movies[index] };
-    movies[index].liked = !movies[index].liked;
-    this.setState({ movies });
+  const fff = ()=>{
+    return handleLike = ()=>{}
+  }
+  const handleLike = (movie) => {
+    // return () => {
+      const newMovies = [...movies];
+      const index = newMovies.indexOf(movie);
+      newMovies[index] = { ...newMovies[index] };
+      newMovies[index].liked = !newMovies[index].liked;
+      console.log("newMovies",newMovies);
+      setMovies(newMovies);
+    // }
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+    // render() {
+      console.log("movies",movies)
+    const count = movies.length;
+    const movies_arr = [...movies]
     if (count === 0) return <p>There are no movies in the database.</p>;
     return (
       <React.Fragment>
-        <p>Showing {count} movies in the database.</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map(movie => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}
+        <div className="container py-5">
+          <div className="row">
+            <p>Showing {count} movies in the database.</p>
+
+            {movies_arr.map((movie) => {
+              return (
+                <div className="col-sm-4" key={movie._id}>
+                  <Card
+                    objectProps={{
+                      _id: movie._id,
+                      title: movie.title,
+                      poster_path:movie.poster_path,
+                      genreName: movie.genre.name,
+                      numberInStock: movie.numberInStock,
+                      dailyRentalRate: movie.dailyRentalRate,
+                      liked: movie.liked,
+                      // handleLikeRef: handleLike,
+                    }}
+                    handleLike={()=>handleLike(movie)}
                   />
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </React.Fragment>
     );
-  }
+  // }
 }
 
 export default Vidly;
