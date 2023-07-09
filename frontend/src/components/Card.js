05300e22d2c6c5ca633c78d8366c28f378393e64
Ajay Thanki <akthanki5@gmail.com>
2023-07-09 10:40:45 +0530

    responsive homepage

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Like from "./Like";
-import { Link, useNavigate } from "react-router-dom";
-import { getGenre } from "../services/genreService";
+import { useNavigate } from "react-router-dom";
+// import { getGenre } from "../services/genreService";
 import { likeMovie } from "../redux/movieSlice";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,14 +9,14 @@ const Card = ({ movie, style = {} }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state?.user);
-  const { title, poster_path, genreIds, popularity, vote_count, liked } = movie;
+  const { title, poster_path, vote_count, liked } = movie;
 
   const isLiked = liked?.find(
     (l) => l.user.username === user?.username
   )?.isLiked;
 
   return (
-    <div className="text-white w-full m-2 justify-center rounded-md hover:scale-105 backdrop-blur-2xl transition-all duration-300">
+    <div className="text-white w-full justify-center rounded-md hover:scale-105 backdrop-blur-2xl transition-all duration-300">
       <img
         style={{ cursor: "pointer", maxHeight: 800, objectFit: "cover", borderRadius:10 }}
         src={poster_path}
@@ -24,17 +24,17 @@ const Card = ({ movie, style = {} }) => {
         alt={title}
         onClick={() => navigate(`/movie/${movie._id}`)}
       />
-      <div className="min-h-[210px] text-center py-3 ">
-        <h2 className="font-bold text-2xl text-center">
-          {title.length>15 ? (title.slice(0, 25) + "...") : title}
+      <div className="sm:min-h-[180px] text-center p-3 sm:py-3 ">
+        <h2 className="font-bold text-xl lg:text-2xl text-center my-2">
+          {/* {title.length>20 ? (title.slice(0, 20) + "...") : title} */title}
         </h2>
-        <div className="px-2 flex justify-around">
-          <div className="flex">
+        {/* <div className="px-2 flex-col md:flex justify-around">
+          <div className="flex sm:text-sm">
+            <div className="flex flex-col">
             Genres:
-            <div className="">
               {genreIds.map((genreId, index) => {
                 return (
-                  <span key={genreId}>
+                  <span key={genreId} className="flex">
                     <Link
                       // key={genreId}
                       to={genreId === 1 ? `/` : `/movies/genre/${genreId}`}
@@ -42,24 +42,16 @@ const Card = ({ movie, style = {} }) => {
                     >
                       {getGenre(genreId).name}
                     </Link>
-                    {genreIds.length - 1 !== index ? " | " : ""}
+                    <span className="hidden lg:flex"> {genreIds.length - 1 !== index ? " | " : ""}</span>
                   </span>
                 );
               })}
             </div>
           </div>
-        </div>
-        <div className="flex justify-around">
-          <span className="stock">Popularity: {popularity}</span>
-          <span className="rate">Votes: {vote_count}</span>
-        </div>
-        <button
-          className="bg-transparent border-0 my-2 text-lg font-medium hover:text-blue-400"
-          onClick={() => navigate(`/movie/${movie._id}`)}
-        >
-          Write an Review
-        </button>
-        <div className=" flex justify-around text-2xl">
+        </div> */}
+        <div className="flex justify-around items-center">
+          <span className="likes">Likes: {vote_count}</span>
+        <div className=" flex justify-around my-2 text-2xl">
           <Like
             liked={isLiked === undefined ? false : isLiked}
             onClick={
@@ -69,6 +61,14 @@ const Card = ({ movie, style = {} }) => {
             }
           />
         </div>
+        </div>
+        <button
+          className="bg-transparent border-0 text-lg font-medium hover:text-blue-400"
+          onClick={() => navigate(`/movie/${movie._id}`)}
+        >
+          Read more
+        </button>
+        
       </div>
     </div>
   );
--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -10,9 +10,9 @@ const Movies = () => {
   return (
     <div className="container mt-2 mx-auto">
       <p className="text-center text-lg text-white font-medium my-4">Showing {moviesList?.length} movies in the database.</p>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-col-1 sm:grid-cols-3 sm:gap-0 lg:gap-0 lg:grid-cols-3 xl:grid-cols-4 xl:gap-4">
         {moviesList?.map((movie) => (
-          <div className="grid-flow-col" key={"card"+movie._id}>
+          <div className="w-96 my-2 sm:w-56 lg:w-80 sm:mx-0 lg:m-2 justify-self-center" key={"card"+movie._id}>
             <Card movie={movie} />
           </div>
         ))}
